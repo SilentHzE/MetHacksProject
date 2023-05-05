68041d75b62022f4389eedccefe1a15de7574e6d
SilentHzE <georgecuix@gmail.com>
2023-05-05 17:34:01 -0400

    test2

--- test.java
+++ test.java
@@ -1,5 +1,8 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
     }
 }
